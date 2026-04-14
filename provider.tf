e47032bc4231f4fc05ab1eb07ca601e37809d02c
Rui Santos <rui.tavares.santos@jeronimo-martins.com>
2026-04-14 13:38:39 +0100

    correct project

--- provider.tf
+++ provider.tf
@@ -1,19 +1,4 @@
-terraform {
-  backend "gcs" {
-    bucket      = "jm-tfremotestate-ye63"
-    prefix      = "aratiendas-com/tfstate"
-    credentials = "./google-key-dns.json"
-  }
-}
-
 provider "google" {
-  credentials = file("./google-key-dns.json")
-  project     = "jm-global-networking-eu"
-  region      = "europe-west1"
-}
-
-provider "google-beta" {
-  credentials = file("./google-key-dns.json")
-  project     = "jm-global-networking-eu"
+  project     = "jm-gcp-pt-jmhrcv-s-dj99d"
   region      = "europe-west1"
 }
